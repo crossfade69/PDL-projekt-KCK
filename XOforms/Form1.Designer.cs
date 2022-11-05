057af8b93ef7ab07910451179f3ddcdc67cbb02c
crossfade69 <73494330+crossfade69@users.noreply.github.com>
2022-11-05 18:58:24 +0100

    Add files via upload

---
+++ XOforms/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace XOforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblkto = new System.Windows.Forms.Label();
+            this.wynikX = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.wynikO = new System.Windows.Forms.Label();
+            this.lblO = new System.Windows.Forms.Label();
+            this.wynik = new System.Windows.Forms.Label();
+            this.restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(168, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 150);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(324, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 150);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(12, 168);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 150);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(168, 168);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 150);
+            this.button5.TabIndex = 3;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(324, 168);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 150);
+            this.button6.TabIndex = 4;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(12, 324);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 150);
+            this.button7.TabIndex = 5;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(168, 324);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 150);
+            this.button8.TabIndex = 6;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(324, 324);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(150, 150);
+            this.button9.TabIndex = 7;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(646, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "RuchGracza:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblkto
+            // 
+            this.lblkto.AutoSize = true;
+            this.lblkto.Location = new System.Drawing.Point(730, 92);
+            this.lblkto.Name = "lblkto";
+            this.lblkto.Size = new System.Drawing.Size(20, 20);
+            this.lblkto.TabIndex = 9;
+            this.lblkto.Text = "O";
+            // 
+            // wynikX
+            // 
+            this.wynikX.AutoSize = true;
+            this.wynikX.Location = new System.Drawing.Point(702, 153);
+            this.wynikX.Name = "wynikX";
+            this.wynikX.Size = new System.Drawing.Size(17, 20);
+            this.wynikX.TabIndex = 11;
+            this.wynikX.Text = "0";
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(675, 153);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(21, 20);
+            this.lblX.TabIndex = 10;
+            this.lblX.Text = "X:";
+            // 
+            // wynikO
+            // 
+            this.wynikO.AutoSize = true;
+            this.wynikO.Location = new System.Drawing.Point(702, 133);
+            this.wynikO.Name = "wynikO";
+            this.wynikO.Size = new System.Drawing.Size(17, 20);
+            this.wynikO.TabIndex = 13;
+            this.wynikO.Text = "0";
+            // 
+            // lblO
+            // 
+            this.lblO.AutoSize = true;
+            this.lblO.Location = new System.Drawing.Point(673, 133);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(23, 20);
+            this.lblO.TabIndex = 12;
+            this.lblO.Text = "O:";
+            // 
+            // wynik
+            // 
+            this.wynik.AutoSize = true;
+            this.wynik.Location = new System.Drawing.Point(669, 113);
+            this.wynik.Name = "wynik";
+            this.wynik.Size = new System.Drawing.Size(55, 20);
+            this.wynik.TabIndex = 14;
+            this.wynik.Text = "WYNIK";
+            // 
+            // restart
+            // 
+            this.restart.Location = new System.Drawing.Point(577, 264);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(269, 73);
+            this.restart.TabIndex = 15;
+            this.restart.Text = "Restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 491);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.wynik);
+            this.Controls.Add(this.wynikO);
+            this.Controls.Add(this.lblO);
+            this.Controls.Add(this.wynikX);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.lblkto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "XO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label1;
+        private Label lblkto;
+        private Label wynikX;
+        private Label lblX;
+        private Label wynikO;
+        private Label lblO;
+        private Label wynik;
+        private Button restart;
+    }
+}
