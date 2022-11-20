2027cde59951e8e791da8913a50cc5f1ef7d985a
crossfade69 <73494330+crossfade69@users.noreply.github.com>
2022-11-20 15:51:00 +0100

    Add files via upload

---
+++ customforms/customforms/Games.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace customforms
+{
+    partial class Games
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.gameliblabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.gameliblabel);
+            this.panel2.Location = new System.Drawing.Point(19, 17);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1272, 500);
+            this.panel2.TabIndex = 17;
+            // 
+            // gameliblabel
+            // 
+            this.gameliblabel.AutoSize = true;
+            this.gameliblabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gameliblabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gameliblabel.ForeColor = System.Drawing.Color.White;
+            this.gameliblabel.Location = new System.Drawing.Point(24, 21);
+            this.gameliblabel.Name = "gameliblabel";
+            this.gameliblabel.Size = new System.Drawing.Size(178, 43);
+            this.gameliblabel.TabIndex = 17;
+            this.gameliblabel.Text = "Your Games";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1311, 533);
+            this.panel1.TabIndex = 18;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 45;
+            this.listBox1.Items.AddRange(new object[] {
+            "amogus",
+            "BeatSaber",
+            "Borderlands",
+            "Borderlands 2",
+            "Borderlands 3",
+            "Borderlands: The Pre-Sequel",
+            "Fallout New Vegas",
+            "Gorilla Tag",
+            "GUILTY GEAR STRIVE",
+            "Heroes of Might & Magic 5",
+            "Keep Talking and Nobody Explodes",
+            "Left4Dead2",
+            "Mass Effect 2",
+            "Neuroshima Hex",
+            "Star Wars: The Old Republic",
+            "Street Fighter III: 3rd Strike",
+            "Street Fighter V: Arcade Edition",
+            "Superhot"});
+            this.listBox1.Location = new System.Drawing.Point(24, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1225, 409);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 18;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Games
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "Games";
+            this.Size = new System.Drawing.Size(1311, 533);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private Panel panel1;
+        private Label gameliblabel;
+        private ListBox listBox1;
+    }
+}
